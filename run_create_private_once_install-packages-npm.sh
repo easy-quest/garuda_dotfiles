bd40a5fb54bf646bd3ef1b5024e2aa75193612fc
Ivan Yastrebov <easy-quest@mail.ru>
2021-11-09 14:18:03 +0300

    Initial commit

--- run_create_private_once_install-packages-npm.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p ~/.npm-global
-npm config set prefix '~/.npm-global'
-printf "export PATH=~/.npm-global/bin:$PATH" >> ~/.profile
-source ~/.profile
-
-npm install -g pyright
