98acb3985259ce15ea402388568dded39ddd326d
Ivan Yastrebov <easy-quest@mail.ru>
2021-11-10 15:56:01 +0300

    Update dot_SpaceVim.d/init.toml Update run_once_install-packages.sh

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo pacman -S rust-analyzer nodejs npm 
+sudo pacman -S rust-analyzer nodejs npm python-pip
 
 npm -g install remark
 npm -g install remark-cli
@@ -8,3 +8,8 @@ npm -g install remark-stringify
 npm -g install remark-frontmatter
 npm -g install wcwidth
 npm install --global prettier
+npm install -g pyright
+
+pip3 install --user pylint yapf autoflake isort coverage 
+
+curl -sLf https://spacevim.org/install.sh | bash -s -- --install neovim
