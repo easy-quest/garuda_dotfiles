225c5e622d7e36a3ff3ce0569013dc0f3a1bde10
Ivan Yastrebov <easy-quest@mail.ru>
2021-11-10 15:50:00 +0300

    Update dot_SpaceVim.d/init.toml Update run_once_install-packages.sh

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -2,3 +2,9 @@
 
 sudo pacman -S rust-analyzer nodejs npm 
 
+npm -g install remark
+npm -g install remark-cli
+npm -g install remark-stringify
+npm -g install remark-frontmatter
+npm -g install wcwidth
+npm install --global prettier
