daeb39a8ff4ecc9d6359602b11efd6bff6098ae4
Ivan Yastrebov <easy-quest@mail.ru>
2021-11-11 03:50:10 +0300

    commit init

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 
-sudo pacman -S rust-analyzer nodejs npm python-pip
-sudo pacman -S postgresql
+# sudo pacman -S rust-analyzer nodejs npm python-pip
+# sudo pacman -S postgresql fish
 
-pamac install direnv
+# pamac install direnv
 
-npm -g install remark
-npm -g install remark-cli
-npm -g install remark-stringify
-npm -g install remark-frontmatter
-npm -g install wcwidth
-npm install --global prettier
-npm install -g pyright
+# npm -g install remark
+# npm -g install remark-cli
+# npm -g install remark-stringify
+# npm -g install remark-frontmatter
+# npm -g install wcwidth
+# npm install --global prettier
+# npm install -g pyright
 
-pip3 install --user pylint yapf autoflake isort coverage pip-tools
+# pip3 install --user pylint yapf autoflake isort coverage pip-tools
 
-curl -sLf https://spacevim.org/install.sh | bash -s -- --install neovim
+curl -L https://github.com/oh-my-fish/oh-my-fish/raw/master/bin/install | fish
+omf update
+# curl -sLf https://spacevim.org/install.sh | bash -s -- --install neovim
