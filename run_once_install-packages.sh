56abd9c926515260eb3d2c2c0452ca4786ff1675
Ivan Yastrebov <easy-quest@mail.ru>
2021-11-11 03:29:38 +0300

    commit init

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 sudo pacman -S rust-analyzer nodejs npm python-pip
+pamac install direnv
 
 npm -g install remark
 npm -g install remark-cli
@@ -10,6 +11,6 @@ npm -g install wcwidth
 npm install --global prettier
 npm install -g pyright
 
-pip3 install --user pylint yapf autoflake isort coverage 
+pip3 install --user pylint yapf autoflake isort coverage pip-tools
 
 curl -sLf https://spacevim.org/install.sh | bash -s -- --install neovim
