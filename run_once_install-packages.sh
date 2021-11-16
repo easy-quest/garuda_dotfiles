3d3a82a35bf885f235522aaa743abc953b2578ad
Ivan Yastrebov <easy-quest@mail.ru>
2021-11-16 16:32:21 +0300

    Update run_once_install-packages.sh

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -1,22 +1,22 @@
-#!/usr/bin/env bash
+#!/usr/bin/sudo bash
 
 # sudo pacman -S rust-analyzer nodejs npm python-pip
 # sudo pacman -S postgresql fish
 
 # pamac install direnv
-yay -S rust-analyzer nodejs npm python-pip fish-done
+pacman -S rust-analyzer nodejs npm python-pip
 
-npm -g install remark
-npm -g install remark-cli
-npm -g install remark-stringify
-npm -g install remark-frontmatter
-npm -g install wcwidth
-npm install --global prettier
-npm install -g pyright
-npm install -g robbyrussell
-npm install -g chalk
+# npm -g install remark
+# npm -g install remark-cli
+# npm -g install remark-stringify
+# npm -g install remark-frontmatter
+# npm -g install wcwidth
+# npm install --global prettier
+# npm install -g pyright
+# npm install -g robbyrussell
+# npm install -g chalk
 
-pip3 install -U --user pip pylint yapf autoflake isort coverage pip-tools
+# pip3 install -U --user pip pylint yapf autoflake isort coverage pip-tools
 
 # curl -L https://github.com/oh-my-fish/oh-my-fish/raw/master/bin/install | fish
 # omf update
